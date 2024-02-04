8af25d695125ecbf278d6d9153c7ae9def9c9ce4
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-02-04 15:59:43 +0600

     List the project IDs, project names, and the total number of assigned employees for each project in descending order of total number of assigned employees.

--- FINAL_TASKS.sql
+++ FINAL_TASKS.sql
@@ -7,3 +7,10 @@ where EID not in (
         where extract(year from AssignmentDate) = 2023
 );
 -- task2
+
+SELECT P.PID, P.PName, COUNT(A.EID) AS TotalAssignedEmployees
+FROM Project P
+JOIN Assignment A ON P.PID = A.PID
+GROUP BY P.PID, P.PName
+ORDER BY TotalAssignedEmployees DESC;
+        
