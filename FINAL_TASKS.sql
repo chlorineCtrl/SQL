d3c6cb5ccf979ed6242264844139e1b258c88752
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-02-04 16:24:53 +0600

    List the names of employees whose average work hours are greater than the average work hours across all employees.

--- FINAL_TASKS.sql
+++ FINAL_TASKS.sql
@@ -1,4 +1,4 @@
--- task1
+-- 2a(i)
 select EName
 from employee
 where EID not in (
@@ -6,11 +6,23 @@ where EID not in (
         from assignment
         where extract(year from AssignmentDate) = 2023
 );
--- task2
+-- 2a(ii)
 
 SELECT P.PID, P.PName, COUNT(A.EID) AS TotalAssignedEmployees
 FROM Project P
 JOIN Assignment A ON P.PID = A.PID
 GROUP BY P.PID, P.PName
 ORDER BY TotalAssignedEmployees DESC;
-        
+
+-- 2a(iii)
+
+SELECT EName
+FROM Employee
+WHERE EID IN (
+    SELECT EID
+    FROM Assignment
+    GROUP BY EID
+    HAVING AVG(HoursWorked) > (SELECT AVG(HoursWorked) FROM Assignment)
+);
+
+
